31b4b3b4f1d03a1cf7b31cd13d4402a27da795ed
ikrom97 <ikromr04@gmail.com>
2022-12-21 16:10:13 +0500

    banners bullet

--- resources/views/components/newslifestyle.blade.php
+++ resources/views/components/newslifestyle.blade.php
@@ -6,7 +6,7 @@
     <div class="newslifestyle-card__description">{!! strip_tags($newslifestyle->description) !!}</div>
   </div>
 
-  <img class="newslifestyle-card__img" src="{{asset('files/newslifestyles/' . $newslifestyle->img)}}" alt="Новость">
+  <img class="newslifestyle-card__img" src="{{ asset('files/newslifestyles/' . $newslifestyle->img) }}" alt="Новость">
 
-  <a class="newslifestyle-card__link" href="{{route('newslifestyle.show', $newslifestyle->slug)}}">Подробнее</a>
+  <a class="newslifestyle-card__link" href="{{ route('newslifestyle.show', $newslifestyle->slug) }}">Подробнее</a>
 </figure>
