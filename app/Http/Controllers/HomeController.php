5128eaa4da51c68512af26d7f2cc68f959ac0c76
ikrom97 <ikromr04@gmail.com>
2023-01-16 10:18:07 +0500

    last commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,6 @@ use App\Helpers\Helper;
 use App\Models\Banner;
 use App\Models\Newslifestyle;
 use App\Models\Product;
-use Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -17,11 +16,6 @@ class HomeController extends Controller
     $data['banners'] = Banner::latest()->get();
 
     $products = Product::get();
-
-    foreach ($products as $product) {
-      $product->slug = SlugService::createSlug(Product::class, 'slug', $product->title);
-      $product->update();
-    }
 
     return view('pages.home.index', compact('data'));
   }
